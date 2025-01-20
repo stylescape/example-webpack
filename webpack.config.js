af8b686840602464bc74219a192e71b585cfe5c4
Lars van Vianen <lars@vianen.com>
2025-01-20 07:38:48 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,34 +21,35 @@ module.exports = {
     ],
     module: {
         rules: [
-        {
-            test: /\.(scss)$/,
-            use: [
             {
-                // Adds CSS to the DOM by injecting a `<style>` tag
-                loader: "style-loader"
-            },
-            {
-                // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                loader: "css-loader"
-            },
-            {
-                // Loader for webpack to process CSS with PostCSS
-                loader: "postcss-loader",
-                options: {
-                postcssOptions: {
-                    plugins: [
-                    autoprefixer
-                    ]
+                test: /\.(scss)$/,
+                use: [
+                {
+                    // Adds CSS to the DOM by injecting a `<style>` tag
+                    loader: "style-loader"
+                },
+                {
+                    // Interprets `@import` and `url()` like `import/require()`
+                    // // and will resolve them
+                    loader: "css-loader"
+                },
+                {
+                    // Loader for webpack to process CSS with PostCSS
+                    loader: "postcss-loader",
+                    options: {
+                    postcssOptions: {
+                        plugins: [
+                        autoprefixer
+                        ]
+                    }
+                    }
+                },
+                {
+                    // Loads a SASS/SCSS file and compiles it to CSS
+                    loader: "sass-loader"
                 }
-                }
-            },
-            {
-                // Loads a SASS/SCSS file and compiles it to CSS
-                loader: "sass-loader"
+                ]
             }
-            ]
-        }
         ]
     }
 }
